bfc5808c03887d749e247cc92bb627cde10406f8
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-04 04:44:53 +0530

    complete table

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Brand from '../Components/Brand'
+import Table from '../Components/Table'
 
 function Dashboard() {
   return (
     <>
         <Navbar />
         <Brand />
+        <Table />
     </>
   )
 }
