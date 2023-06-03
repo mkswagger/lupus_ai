6e44bc3836a5d2220b4e913a1bc288c5868139ab
flaminshinjan <srikpatra.2008@gmail.com>
2023-06-03 16:58:40 +0530

    initial setup done

---
+++ lupus_app/lib/screens/dashboard1.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StatisticsPage extends StatelessWidget {
+  const StatisticsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
