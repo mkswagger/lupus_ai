553a8231528793677dd1b4fff16ad4c13a95aced
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-04 12:12:13 +0530

    minor changes

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -7,24 +7,27 @@ function Table() {
         { id: 2, amount: 50, date: '2022-06-02T14:30:00Z' },
         { id: 3, amount: 200, date: '2022-06-03T12:15:00Z' },
         { id: 4, amount: 75, date: '2022-06-04T08:45:00Z' },
+        { id: 5, amount: 100, date: '2022-06-01T10:00:00Z' },
+        
+        
       ];
 
   return (
-    <div className="overflow-x-hidden bg-gray-900">
+    <div className="overflow-x-hidden  ">
     <table className="table-fixed w-full sm:w-auto md:w-screen lg:w-screen">
       <thead>
         <tr>
-          <th className="w-1/3 px-4 py-2 text-left bg-gray-900 text-white">Transaction ID</th>
-          <th className="w-1/3 px-4 py-2 text-left bg-gray-900 text-white">Amount</th>
-          <th className="w-1/3 px-4 py-2 text-left bg-gray-900 text-white">Date &amp; Time</th>
+          <th className="w-1/3 h-36 px-4 py-2 text-left bg-gray-900 text-white">Transaction ID</th>
+          <th className="w-1/3 h-36 px-4 py-2 text-left bg-gray-900 text-white">Amount</th>
+          <th className="w-1/3 h-36 px-4 py-2 text-left bg-gray-900 text-white">Date &amp; Time</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className=''>
         {transactions.map((transaction) => (
           <tr key={transaction.id} className="bg-gray-900">
-            <td className="w-1/3 px-4 py-2 text-white">{transaction.id}</td>
-            <td className="w-1/3 px-4 py-2 text-white">{transaction.amount}</td>
-            <td className="w-1/3 px-4 py-2 text-white">{new Date(transaction.date).toLocaleString()}</td>
+            <td className="w-1/3 h-16 px-4 py-2 text-white">{transaction.id}</td>
+            <td className="w-1/3 h-16 px-4 py-2 text-white">{transaction.amount}</td>
+            <td className="w-1/3 h-16 px-4 py-2 text-white">{new Date(transaction.date).toLocaleString()}</td>
           </tr>
         ))}
       </tbody>
