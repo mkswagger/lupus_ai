570e80427a90e38a0713b069e63b172fd9374ef6
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-04 18:23:10 +0530

    fix nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ import logout from '../assets/logout.png'
 function Navbar() {
   return (
     <>
-        <div className='flex flex-row justify-between w-screen h-14 bg-[#000113] items-center pl-5 sm:pl-5 md:pl-10'>
+         <div className='flex flex-row justify-between w-screen h-14 bg-[#000113] items-center pl-5 sm:pl-5 md:pl-10'>
             <div className='flex h-full items-center'>
                 <img src={logo} alt="logo" className='h-full pt-2 pb-2 w-auto'/>
                 <p className='font-bold text-white pl-4'>LUPUS.AI</p>
@@ -21,8 +21,8 @@ function Navbar() {
                     <p className='font-bold text-white pr-7 hidden sm:hidden md:block hover:text-[#8233A8]'>Settings</p>
                 </div>
                 <div className='flex h-full items-center cursor-pointer'>
-                    <img src={logout} alt="support" className='h-full pt-5 pb-5 w-auto pr-4 sm:pr-4 md:pr-0'/>
-                    <p className='font-bold text-white pr-2 sm:pr-2 pl-2 sm:pl-2 md:pr-10 hidden sm:hidden md:block hover:text-[#8233A8]'>Log Out</p>
+                    <img src={logout} alt="support" className='h-full pt-5 pb-5 w-auto mr-4 sm:mr-4 md:mr-0'/>
+                    <p className='font-bold text-white mr-2 sm:mr-2 ml-2 sm:ml-2 md:mr-10 hidden sm:hidden md:block hover:text-[#8233A8]'>Log Out</p>
                 </div>
             </div>
         </div>
